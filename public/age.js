3aa4af08645151b09f9732e4aa3a33486cf38ee0
adhithya5599 <adhithya5599@gmail.com>
2020-07-02 22:14:12 +0530

    Changed

--- public/age.js
+++ public/age.js
@@ -1,4 +1,10 @@
-var div = document.querySelector("div");
-div.addEventListener("click",(e) => {
-	console.log(e.target);
-})
+var div = document.querySelector("#main");
+var yearToday = new Date().getFullYear();
+
+div.addEventListener("click",function(e){
+	var each = e.target.parentNode;
+	var bDay = Number.parseInt(each.children[1].textContent);
+	var tag = document.createElement("p"); 
+	tag.innerHTML = "The age in years "+(yearToday-bDay)+". The age in months "+((yearToday-bDay)*12)+". The age in days "+((yearToday-bDay)*365.25);
+	each.appendChild(tag);
+});
